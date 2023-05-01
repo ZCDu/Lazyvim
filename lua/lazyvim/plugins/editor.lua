20c8eb01c9d0109ea03d332d064c7f1a31365ce8
ZCDu <ubuntu@wsl.com>
2023-05-01 14:28:00 +0800

    neo-tree delete cwd keymap

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -14,13 +14,6 @@ return {
         end,
         desc = "Explorer NeoTree (root dir)",
       },
-      {
-        "N",
-        function()
-          require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
-        end,
-        desc = "Explorer NeoTree (cwd)",
-      },
       { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
       { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)",      remap = true },
     },
